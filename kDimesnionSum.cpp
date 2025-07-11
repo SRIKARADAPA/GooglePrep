c56bff388a1c938e412c41093ebd56c50fbaabb1
Srikar Adapa <venkadap@amazon.com>
2025-07-11 19:02:31 +0530

    solved in iterative approach

--- kDimesnionSum.cpp
+++ kDimesnionSum.cpp
@@ -3,26 +3,35 @@
 using namespace std;
 int sum=0;
 int getElement(vector<int> &indices){
+    for(int i=0;i<indices.size();i++){
+        cout<<indices[i]<<" ";
+    }
+    cout<<"\n";
     return 1;
 }
-void findSum(vector<int> &dimensions, int index){
-    sum+=getElement(dimensions);
-    for(int i=index;i<dimensions.size();i++){
-        if(dimensions[i]==0){
-            continue;
+void findSum(vector<int> &dimensions){
+    bool isFinished = false;
+    int n = dimensions.size();
+    vector<int> indices(n,0);
+    while(!isFinished){
+        sum+=getElement(indices);
+        int i = n-1;
+        while(i>=0){
+            indices[i]++;
+            if(indices[i]<dimensions[i]){
+                break;
+            }
+            indices[i]=0;
+            i--;
         }
-        dimensions[i]--;
-        findSum(dimensions, i);
-        dimensions[i]++;
+        if(i<0){
+            isFinished = true;
+        }
     }
     return;
 }
 int main(){
-    vector<int> dimensions = {2,3,7,3};
-    for(int i=0;i<dimensions.size();i++){
-        dimensions[i]--;
-    }
-    findSum(dimensions,0);
-    cout<<sum<<" ";
+    vector<int> dimensions = {2,2,4,4};
+    findSum(dimensions);
     return 0;
 }
